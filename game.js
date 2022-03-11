93e1cc1144b3a0ad98b6f41fd31048d8c19eb6eb
Cuong Ly <cuonglyy@gmail.com>
2022-03-11 15:26:56 -0800

    Able to play a round with the computer

--- game.js
+++ game.js
@@ -3,19 +3,36 @@ let computerPlay = () => {
   // Use numbers to determine output
   // 0 = Rock, 1 = Paper, 2 = Scissors
   let decision = Math.floor( Math.random() * 3 );
-  if ( decision === 0 ) {
-    return "rock";
-  } else if ( decision === 1) {
-    return "paper";
-  } else {
-    return "scissor";
+  switch (decision) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
   }
 }
 
 console.log( computerPlay() );
 
-let playerSelection = "rOCk";
-let computerSelection = computerPlay();
 let playRound = (playerSelection, computerSelection) => {
-  
+  playerSelection = playerSelection.toLowerCase();
+
+  // If player selects rock and comp selects paper, comp wins
+  // If player selects rock and comp selects scissor, player wins
+  // If player selects rock and comp selects rock, it's a tie
+
+  if ( playerSelection === "rock" ) {
+    if ( computerSelection === "rock") {
+      return "Tie!";
+    } else if ( computerSelection === "paper") {
+      return "You Lose! Paper beats Rock.";
+    } else {
+      return "You Win! Rock beats Scissor.";
+    }
+  }
 }
+
+let playerSelection = "rOCk";
+let computerSelection = computerPlay().toLowerCase();
+console.log(playRound(playerSelection, computerSelection))
