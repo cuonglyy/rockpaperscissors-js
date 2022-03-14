9da2ba7b236ee85d1eab4483a72a399a648f58e0
Cuong Ly <cuonglyy@gmail.com>
2022-03-14 15:56:36 -0700

    Can play 5 rounds and highest points is the winnagit add --all

--- game.js
+++ game.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 let computerPlay = () => {
   // Computer will randomly return rock, paper, or scissors
   // Use numbers to determine output
@@ -9,30 +12,47 @@ let computerPlay = () => {
     case 1:
       return "paper";
     case 2:
-      return "scissors";
+      return "scissor";
   }
 }
-
-console.log( computerPlay() );
 
 let playRound = (playerSelection, computerSelection) => {
+  playerSelection = prompt("Rock, paper, or scissor?");
+  computerSelection = computerPlay().toLowerCase();
+  //console.log(computerSelection)
   playerSelection = playerSelection.toLowerCase();
+  //console.log(playerSelection);
 
   // If player selects rock and comp selects paper, comp wins
-  // If player selects rock and comp selects scissor, player wins
-  // If player selects rock and comp selects rock, it's a tie
+  // If player selects paper and comp selects scissor, comp wins
+  // If player selects scissor and comp selects rock, comp wins
 
-  if ( playerSelection === "rock" ) {
-    if ( computerSelection === "rock") {
-      return "Tie!";
-    } else if ( computerSelection === "paper") {
-      return "You Lose! Paper beats Rock.";
+  if (playerSelection == computerSelection) {
+    return "Tie!";
+  } else if (
+    (playerSelection == "rock" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "scissor") || (playerSelection == "scissor" && computerSelection == "rock") ) {
+      computerScore++;
+      return "Computer wins!";
     } else {
-      return "You Win! Rock beats Scissor.";
+      playerScore++;
+      return "You win!";
     }
+}
+
+let game = () => {
+
+  for (let i = 0; i < 5; i++) {
+    console.log( playRound() );
+    console.log("Computer Score: " + computerScore);
+    console.log("Player Score: " + playerScore);
+  }
+  if (computerScore > playerScore) {
+    return "You lose )-:";
+  } else if (computerScore == playerScore) {
+    return "Tie game!"
+  } else {
+    return "You Win! :D"
   }
 }
 
-let playerSelection = "rOCk";
-let computerSelection = computerPlay().toLowerCase();
-console.log(playRound(playerSelection, computerSelection))
+console.log( game() );
